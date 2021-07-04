19856109539e370a2f799abe2ac6d0128d9111f7
ayushi2.sharma <Ayushi2.Sharma@ril.com>
2021-07-04 15:08:40 +0530

    Initial commit - Initialization of project

---
+++ src/main/java/com/example/machineCoding/MachineCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.machineCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineCodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/machineCoding/MachineCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.machineCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
