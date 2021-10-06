78bafcd8f26953c41bb166d40b9de3fc098d644a
ayushi2.sharma <Ayushi2.Sharma@ril.com>
2021-10-06 23:59:11 +0530

    roll method updated

--- src/main/java/com/example/machineCoding/model/Dice.java
+++ src/main/java/com/example/machineCoding/model/Dice.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @Getter
 @AllArgsConstructor
 //No need for Setter since all fields are immutable so no need to set the values.
@@ -16,5 +18,6 @@ public class Dice {
         //minValue included : 1
         //maxValue excluded, so maxValue + 1 : 7
         return RandomUtils.nextInt(minValue, maxValue + 1); //dice number from 1 to 6 (included)
+        //return ThreadLocalRandom.current().nextInt(minValue, maxValue+1);
     }
 }
